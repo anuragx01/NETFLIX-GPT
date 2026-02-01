fa677c9f823947a8adf743f55cdc0d75b798229a
Anurag Raj <anurag2003raj@gmail.com>
2026-02-02 01:54:37 +0530

    first commit

---
+++ Desktop/netflix-gpt/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <div className="text-3xl font-bold text-green-800">
+      hello welcome kr rhe hai   
+   </div>
+  );
+}
+
+export default App;
